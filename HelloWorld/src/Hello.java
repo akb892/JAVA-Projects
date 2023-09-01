658c1801a49b412990c8b6909e803a768095a829
akb892 <aryanin2002@gmail.com>
2023-09-01 12:36:55 +0530

    First Hello

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+     public static void main(String[] args) {
+          System.out.println("Hello, Aryan");
+
+          int myFirstNumber = (5 + 10) + (2 * 10);
+          int mySecondNumber = 12 ,myThirdNumber = myFirstNumber * 2 ;
+          int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+          int myLastOne = 1000 - myTotal;
+          System.out.println(myLastOne);
+          System.out.println(myFirstNumber);
+          System.out.println(mySecondNumber);
+          System.out.println(myThirdNumber);
+          System.out.println(myTotal);
+
+     }
+}
